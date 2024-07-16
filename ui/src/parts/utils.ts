8f8260b062e2ef851df659022a79cc85eb43a0fa
unknown <vml2@cin.ufpe.br>
2024-07-16 14:51:10 -0300

    teste modificacao filtro

--- ui/src/parts/utils.ts
+++ ui/src/parts/utils.ts
@@ -6,23 +6,7 @@ import { border, cssVar } from "../theme"
 export function fuzzysearch(haystack: S, needle: S) {
   haystack = haystack.toLowerCase()
   needle = needle.toLowerCase()
-  const
-    hlen = haystack.length,
-    nlen = needle.length
-
-  if (nlen > hlen) return false
-  if (nlen === hlen) return needle === haystack
-
-  outer: for (let i = 0, j = 0; i < nlen; i++) {
-    const nch = needle.charCodeAt(i)
-    while (j < hlen) {
-      if (haystack.charCodeAt(j++) === nch) {
-        continue outer
-      }
-    }
-    return false
-  }
-  return true
+  return haystack.includes(needle);
 }
 
 // https://github.com/h2oai/wave/issues/1395.

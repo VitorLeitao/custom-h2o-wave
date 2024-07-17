b95a5c57459bb5f626888fa71fc553e81413750b
Beatriz de Araújo Andrade <80129016+BeatrizA2@users.noreply.github.com>
2024-07-17 08:46:29 -0300

    Add setup.py for pip installation

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='custom_h2o_wave',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+    ],
+    author='',
+    author_email='',
+    description='Custom H2O Wave library',
+    url='https://github.com/VitorLeitao/custom-h2o-wave',
+)
